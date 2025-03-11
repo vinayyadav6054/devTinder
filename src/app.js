2d87ca9ff530b0403d8c494c9ea54f18293f7838
vinayyadav6054 <yvinay7271@gmail.com>
2025-03-11 16:07:20 +0530

    Created a express server

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+//This function is called request handler
+app.use('/', (req, res) => {
+    res.send("Hello from the dashbaord!!")
+})
+
+app.use("/hello",(req, res) => {
+    res.send("Hello hello hello!!")
+})
+
+app.use("/test",(req, res) => {
+    res.send("Hello from the server")
+})
+
+
+//Createa a server which is running on 3000 port
+app.listen(3000, () => {
+    console.log(`Server is successfully listening on port 3000`);
+    
+});
