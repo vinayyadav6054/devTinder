34e63bd87f147e66c9190409c0e57294b99c2896
vinayyadav6054 <yvinay7271@gmail.com>
2025-03-12 16:56:41 +0530

    Middleware Chain

--- src/app.js
+++ src/app.js
@@ -4,32 +4,23 @@ const app = express();
 const PORT = 3000;
 
 //This will only handle Get call to /user
+
+//GET /users --> midleware chain ==> request handler 
 app.use("/user",
     (req, res, next) => {
     console.log("Handling the route user!!");
     //res.send("Response!!!");
     next();
    },
-    (req, res,next) =>{
-        console.log("Handling the route user2!!");     
-        //res.send("Response2!!!");
-        next();
-   },
-   (req, res,next) =>{
-        console.log("Handling the route user3!!");     
-        //res.send("Response3!!!");
-        next();
-    },
-        (req, res,next) =>{
-        console.log("Handling the route user4!!");   
-        next();  
-        //res.send("Response4!!!"); 
-    },
-    (req, res, next) => {
-        console.log("Handling the route user5!!");
-        res.send("Response5!!!")
-    }
-   
+);
+
+app.get("/user", (req, res,next) =>{
+    console.log("Handling the route user!!1");
+    next();
+},
+(req, res) => {
+    res.send("Response!!!");
+}
 )
 
 //Createa a server which is running on 3000 port
