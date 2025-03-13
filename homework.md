e89d0fd38c8d14eecb57ec25a51a77fc64bae14e
vinayyadav6054 <yvinay7271@gmail.com>
2025-03-13 14:09:21 +0530

    MiddleWare of admin

--- homework.md
+++ homework.md
@@ -27,3 +27,8 @@ What is the use of "-g" while npm install
 -next()
 -next function and errors along with res.send()
 -app.use("/route", rH,[rH2, rH3], rH4, rH5)
+-What is a Middleware
+-How express JS basically handles requests behind the scenes
+-Difference app.use and app.all
+-Write a dummy auth middleware for admin
+-Write a dummy auth middleware for all user routes, except /user/login
--- src/app.js
+++ src/app.js
@@ -3,27 +3,35 @@ const app = express();
 
 const PORT = 3000;
 
-//This will only handle Get call to /user
+//Handle Auth Middleware for all request GET,POSt,PUT,DELETE
+app.get("/admin",(req, res, next) => {
+    const token = "xyz";
+    const isAdminAuthorised  = token === "xyz"
+     if(! isAdminAuthorised){
+        res.status(401).send("Unauthorized Access");
+     }
+     else{
+        next()
+;     }
+})
 
-//GET /users --> midleware chain ==> request handler 
-app.use("/user",
-    (req, res, next) => {
-    console.log("Handling the route user!!");
-    //res.send("Response!!!");
-    next();
-   },
-);
+app.get("/admin/getAllData", (req, res) => {
+    //logic of Check if the request is authorized
+    
+    if (isAdminAuthorised){
+        res.send("All Data Sent");
+    }
+    else{
+        res.status(401).send("Unauthorized Access");
+    }
 
-app.get("/user", (req, res,next) =>{
-    console.log("Handling the route user!!1");
-    next();
-},
-(req, res) => {
-    res.send("Response!!!");
-}
-)
+})
 
-//Createa a server which is running on 3000 port
+app.get("/admin/deleteUseeer", (req, res) => {
+    //logic of Check if the request is authorized
+    res.send("User Deleted");
+})
+
 app.listen(PORT, () => {
     console.log(`Server is successfully listening on port ${PORT}`);
     
